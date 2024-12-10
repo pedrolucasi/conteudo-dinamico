b5323222afe221c80035ba23722e1ccfc9691f40
Pedro Lucas Silva Batista <20232370030@ad.ifpb.edu.br>
2024-12-09 19:35:11 -0600

    commit final

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ const main = document.querySelector('main.row')
 
 for (const country of flags) {
     const view = `<div class="flag col-2  my-2 text-center">
-        <img scr="https://cdn.jsdelivr.net/gh/hampusborgos/country-flags@main/svg/${country.id}.svg" alt="${country.name}">
+        <img src="${country.image}" alt="${country.name}">
+       
     <p>${country.name}</p>
     </div>`;
 
-    main.insertAdjacentHTML ('beforeend', view);
+    main.insertAdjacentHTML('beforeend', view);
     
 }
 
